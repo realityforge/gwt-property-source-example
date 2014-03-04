6d4287b029e1f2b0acc2eaac8b55c8598030c62d
Peter Donald <peter@realityforge.org>
2014-03-05 10:14:11 +1100

    Style code

--- src/main/java/org/realityforge/gwt/propertysource/example/client/MyStaticPropertySource.java
+++ src/main/java/org/realityforge/gwt/propertysource/example/client/MyStaticPropertySource.java
@@ -7,38 +7,40 @@ import com.github.legioth.propertysource.client.annotations.Property;
 import java.util.List;
 
 @Namespace("foo")
-public interface MyStaticPropertySource extends PropertySource {
-    boolean bool();
+public interface MyStaticPropertySource
+  extends PropertySource
+{
+  boolean bool();
 
-    String string();
+  String string();
 
-    String singleConf();
+  String singleConf();
 
-    List<String> multiConf();
+  List<String> multiConf();
 
-    @Property("foo.string")
-    @BooleanConversion(truePattern = ".*[1,3]")
-    boolean isString1or3();
+  @Property("foo.string")
+  @BooleanConversion(truePattern = ".*[1,3]")
+  boolean isString1or3();
 
-    @Property("user.agent")
-    String userAgent();
+  @Property("user.agent")
+  String userAgent();
 
-    @Property("user.agent")
-    @BooleanConversion(trueValues = { "gecko1_8", "safari" })
-    boolean isOpenSourceEngine();
+  @Property("user.agent")
+  @BooleanConversion(trueValues = { "gecko1_8", "safari" })
+  boolean isOpenSourceEngine();
 
-    @Property("user.agent")
-    @BooleanConversion(truePattern = "ie\\d+")
-    boolean isIE();
+  @Property("user.agent")
+  @BooleanConversion(truePattern = "ie\\d+")
+  boolean isIE();
 
-    @Property("foo.singleConf")
-    List<String> singleAsList();
+  @Property("foo.singleConf")
+  List<String> singleAsList();
 
-    @Property("foo.multiConf")
-    @BooleanConversion(trueValues = { "Conf1", "Conf2" })
-    boolean multiContains1or2();
+  @Property("foo.multiConf")
+  @BooleanConversion(trueValues = { "Conf1", "Conf2" })
+  boolean multiContains1or2();
 
-    @Property("foo.multiConf")
-    @BooleanConversion(trueValues = { "Conf1", "Conf2" }, matchAll = true)
-    boolean multiContains1and2();
+  @Property("foo.multiConf")
+  @BooleanConversion(trueValues = { "Conf1", "Conf2" }, matchAll = true)
+  boolean multiContains1and2();
 }
