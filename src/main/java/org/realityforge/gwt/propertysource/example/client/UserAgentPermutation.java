70554136c207ca6ac69234e4bc7d01d5c7b6b35e
Peter Donald <peter@realityforge.org>
2014-03-05 10:12:25 +1100

    Format and remove unnecessary qualifiers

--- src/main/java/org/realityforge/gwt/propertysource/example/client/UserAgentPermutation.java
+++ src/main/java/org/realityforge/gwt/propertysource/example/client/UserAgentPermutation.java
@@ -5,80 +5,95 @@ import com.github.legioth.propertysource.client.annotations.BooleanConversion;
 import com.github.legioth.propertysource.client.annotations.Property;
 import com.google.gwt.core.shared.GWT;
 
-public class UserAgentPermutation {
+public class UserAgentPermutation
+{
+  @Property("user.agent")
+  interface Properties
+    extends PropertySource
+  {
+    @BooleanConversion(truePattern = "gecko.*")
+    boolean isGecko();
 
-    @Property("user.agent")
-    interface Properties extends PropertySource {
-        @BooleanConversion(truePattern = "gecko.*")
-        public boolean isGecko();
+    @BooleanConversion(trueValues = "safari")
+    boolean isSafari();
 
-        @BooleanConversion(trueValues = "safari")
-        public boolean isSafari();
+    @BooleanConversion(trueValues = "opera")
+    boolean isOpera();
 
-        @BooleanConversion(trueValues = "opera")
-        public boolean isOpera();
+    @BooleanConversion(truePattern = "ie\\d+")
+    boolean isIe();
 
-        @BooleanConversion(truePattern = "ie\\d+")
-        public boolean isIe();
+    @BooleanConversion(trueValues = "ie6")
+    boolean isIe6();
 
-        @BooleanConversion(trueValues = "ie6")
-        public boolean isIe6();
+    @BooleanConversion(trueValues = "ie8")
+    boolean isIe8();
 
-        @BooleanConversion(trueValues = "ie8")
-        public boolean isIe8();
+    @BooleanConversion(trueValues = "ie9")
+    boolean isIe9();
 
-        @BooleanConversion(trueValues = "ie9")
-        public boolean isIe9();
+    String name();
+  }
 
-        public String name();
+  private static Properties devModeCache;
+
+  private static Properties get()
+  {
+    if ( GWT.isProdMode() )
+    {
+      // GWT compiler inlines these, but it could inline if the null check
+      // was here
+      return GWT.create( Properties.class );
     }
-
-    private static Properties devModeCache;
-
-    private static Properties get() {
-        if (GWT.isProdMode()) {
-            // GWT compiler inlines these, but it could inline if the null check
-            // was here
-            return GWT.create(Properties.class);
-        } else {
-            // Use same instance every time in devmode to avoid creating new
-            // object every time
-            if (devModeCache == null) {
-                devModeCache = GWT.create(Properties.class);
-            }
-            return devModeCache;
-        }
+    else
+    {
+      // Use same instance every time in devmode to avoid creating new
+      // object every time
+      if ( devModeCache == null )
+      {
+        devModeCache = GWT.create( Properties.class );
+      }
+      return devModeCache;
     }
+  }
 
-    public static boolean isGecko() {
-        return get().isGecko();
-    }
+  public static boolean isGecko()
+  {
+    return get().isGecko();
+  }
 
-    public static boolean isSafari() {
-        return get().isSafari();
-    }
+  public static boolean isSafari()
+  {
+    return get().isSafari();
+  }
 
-    public static boolean isOpera() {
-        return get().isOpera();
-    }
+  public static boolean isOpera()
+  {
+    return get().isOpera();
+  }
 
-    public static boolean isIe() {
-        return get().isIe();
-    }
+  public static boolean isIe()
+  {
+    return get().isIe();
+  }
 
-    public static boolean isIe6() {
-        return get().isIe6();
-    }
+  public static boolean isIe6()
+  {
+    return get().isIe6();
+  }
 
-    public static boolean isIe8() {
-        return get().isIe8();
-    }
+  public static boolean isIe8()
+  {
+    return get().isIe8();
+  }
 
-    public static boolean isIe9() {
-        return get().isIe9();
-    }
+  public static boolean isIe9()
+  {
+    return get().isIe9();
+  }
 
-    public static String getName() {
-        return get().name();
-    }
+  public static String getName()
+  {
+    return get().name();
+  }
 }
